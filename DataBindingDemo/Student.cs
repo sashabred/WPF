4d2fa3c9217809ae1f13a2712fb719c34392f283
Alexandra Sandanova <slowpoche@gmail.com>
2022-01-30 11:11:47 +0800

    Add 9exc

---
+++ DataBindingDemo/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingDemo
+{
+    class Student
+    {
+        public string StudentName { get; set; }
+        public bool IsEnrolled { get; set; }
+    }
+
+    class StudentList:ObservableCollection<Student>
+    {
+
+    }
+
+
+}
